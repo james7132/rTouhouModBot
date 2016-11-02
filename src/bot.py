a0ea516363260478b82d2b229817c506dc4895fd
james713 <contact@jamessliu.com>
2016-11-02 00:35:51 +0000

    Add distinguishing different posts

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,7 @@
 import praw
 import re
 import time
+from enum import Enum, unique
 from model import * 
 from util import *
 from urllib.parse import urlparse
@@ -17,13 +18,55 @@ domain_whitelist = load_regex_list("./domain_whitelist.txt")
 # A list of domains that are sure to be image rehosts
 domain_blacklist = load_regex_list("./domain_blacklist.txt")
 
+completed = set()
+to_watch = list()
+
+class Decision(Enum):
+    other = 1               # this post is not subject to moderation
+    watch = 2               # watch this post for further changes
+    found_fanart = 3        # post is a piece of found fanart
+    original_content = 4    # post is original content
+    invalid_source = 5      # post is found fanart with improper sourcing
+
+def check_list(check, regex_list):
+    return any(pattern.match(check) for pattern in regex_list)
+
+def check_flair(post):
+    if post.link_flair_text is None:
+        return Decision.watch 
+    flair = post.link_flair_text
+    if(check_list(flair, flair_whitelist)):
+        return Decision.original_content
+    elif(check_list(flair, flair_blacklist)):
+        return Decision.found_fanart
+    return Decision.other
+
+def check_domain(post, flair_result):
+    hostname = urlparse(post.url).hostname;
+    if(check_list(hostname, domain_blacklist)):
+        return Decision.invalid_source
+    return flair_result
+
+def check_post(post):
+    url = urlparse(post.url)
+    result = None 
+    reason = None
+    flair = post.link_flair_text
+
+    flair_check = check_flair(post)
+    if flair_check is not Decision.found_fanart:
+        return flair_check
+
+    # flair_check assumed to be false here
+    return check_domain(post, flair_check)
+
 def main():
     #TODO(james7132): Switch to proper logging
-    print("Flair Whitelist: ", flair_whitelist)
-    print("Fliar Blacklist: ", flair_blacklist)
+    print("Flair Whitelist: ", [regex.pattern for regex in flair_whitelist])
+    print("Fliar Blacklist: ", [regex.pattern for regex in flair_blacklist])
 
-    print("Domain Whitelist: ", domain_whitelist)
-    print("Domain Blacklist: ", domain_blacklist)
+    print("Domain Whitelist: ", [regex.pattern for regex in domain_whitelist ])
+    print("Domain Blacklist: ", [regex.pattern for regex in domain_blacklist ])
 
     #TODO(james7132): Switch to OAuth2 based authentication
     reddit = praw.Reddit('/r/touhou moderator written by /u/james7132 in PRAW')
@@ -33,14 +76,11 @@ def main():
     subreddit = reddit.get_subreddit('touhou')
     print('Subreddit: {0}'.format(subreddit))
     while True:
-        for submission in subreddit.get_new():
-            url = urlparse(submission.url)
-            if(submission.link_flair_text is not None):
-                print(submission.link_flair_text,url.hostname,
-                        any(pattern.match(url.hostname) for pattern in
-                            domain_blacklist),
-                        any(pattern.match(url.hostname) for pattern in
-                                domain_whitelist))
+        for post in subreddit.get_new():
+            if(post.id in completed):
+                continue
+            print(post.link_flair_text, urlparse(post.url).hostname,
+                    check_post(post))
         time.sleep(60)
 
 if __name__ == "__main__":
